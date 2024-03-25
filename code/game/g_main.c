3751a3b579f7204910f09c446bcdfcf80a6e2615
Kevin Remisoski <kremisoski@gmail.com>
2024-03-25 01:14:03 -0400

    some bugfixes

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -310,57 +310,58 @@ G_SpawnWeapon
 void G_SpawnWeapon ( gclient_t *client ) {
 	client->ps.ammo[ WP_MACHINEGUN ] = getAmmoValue ( "g_startAmmoMG" );
 
-	if ( g_removeweapon.integer & 1 && !( g_wpflags.integer & 1 ) ) {
-		client->ps.stats[ STAT_WEAPONS ] &= ~( 1 << WP_MACHINEGUN );
-		client->ps.ammo[ WP_MACHINEGUN ] = 0;
+	if( g_instagib.integer ) {
+		g_wpflags.integer = 32;
 	}
-	if ( g_wpflags.integer & 2 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_SHOTGUN;
-		client->ps.ammo[ WP_SHOTGUN ] = getAmmoValue ( "g_startAmmoSG" );
-	}
-	if ( g_wpflags.integer & 4 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_GRENADE_LAUNCHER;
-		client->ps.ammo[ WP_GRENADE_LAUNCHER ] = getAmmoValue ( "g_startAmmoGL" );
-	}
-	if ( g_wpflags.integer & 8 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_ROCKET_LAUNCHER;
-		client->ps.ammo[ WP_ROCKET_LAUNCHER ] = getAmmoValue ( "g_startAmmoRL" );
-	}
-	if ( g_wpflags.integer & 16 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_LIGHTNING;
-		client->ps.ammo[ WP_LIGHTNING ] = getAmmoValue ( "g_startAmmoLG" );
-	}
-	if ( g_wpflags.integer & 32 ) {
-		if ( g_instagib.integer < 1) {
+
+	else {
+		if ( g_removeweapon.integer & 1 && !( g_wpflags.integer & 1 ) ) {
+			client->ps.stats[ STAT_WEAPONS ] &= ~( 1 << WP_MACHINEGUN );
+			client->ps.ammo[ WP_MACHINEGUN ] = 0;
+		}
+		if ( g_wpflags.integer & 2 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_SHOTGUN;
+			client->ps.ammo[ WP_SHOTGUN ] = getAmmoValue ( "g_startAmmoSG" );
+		}
+		if ( g_wpflags.integer & 4 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_GRENADE_LAUNCHER;
+			client->ps.ammo[ WP_GRENADE_LAUNCHER ] = getAmmoValue ( "g_startAmmoGL" );
+		}
+		if ( g_wpflags.integer & 8 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_ROCKET_LAUNCHER;
+			client->ps.ammo[ WP_ROCKET_LAUNCHER ] = getAmmoValue ( "g_startAmmoRL" );
+		}
+		if ( g_wpflags.integer & 16 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_LIGHTNING;
+			client->ps.ammo[ WP_LIGHTNING ] = getAmmoValue ( "g_startAmmoLG" );
+		}
+		if ( g_wpflags.integer & 32 ) {
 			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_RAILGUN;
 			client->ps.ammo[ WP_RAILGUN ] = getAmmoValue ( "g_startAmmoRG" );
-		} else {
-			client->ps.stats[STAT_WEAPONS] = ( 1 << WP_RAILGUN );
-			client->ps.ammo[WP_RAILGUN] = 999;
 		}
-	}
-	if ( g_wpflags.integer & 64 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_PLASMAGUN;
-		client->ps.ammo[ WP_PLASMAGUN ] = getAmmoValue ( "g_startAmmoPG" );
-	}
-	if ( g_wpflags.integer & 128 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_BFG;
-		client->ps.ammo[ WP_BFG ] = getAmmoValue ( "g_startAmmoBFG" );
-	}
+		if ( g_wpflags.integer & 64 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_PLASMAGUN;
+			client->ps.ammo[ WP_PLASMAGUN ] = getAmmoValue ( "g_startAmmoPG" );
+		}
+		if ( g_wpflags.integer & 128 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_BFG;
+			client->ps.ammo[ WP_BFG ] = getAmmoValue ( "g_startAmmoBFG" );
+		}
 #ifdef MISSIONPACK
-	if ( g_wpflags.integer & 256 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_NAILGUN;
-		client->ps.ammo[ WP_NAILGUN ] = getAmmoValue ( "g_startAmmoNG" );
-	}
-	if ( g_wpflags.integer & 512 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_PROX_LAUNCHER;
-		client->ps.ammo[ WP_PROX_LAUNCHER ] = getAmmoValue ( "g_startAmmoPL" );
-	}
-	if ( g_wpflags.integer & 1024 ) {
-		client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_CHAINGUN;
-		client->ps.ammo[ WP_CHAINGUN ] = getAmmoValue ( "g_startAmmoCG" );
-	}
+		if ( g_wpflags.integer & 256 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_NAILGUN;
+			client->ps.ammo[ WP_NAILGUN ] = getAmmoValue ( "g_startAmmoNG" );
+		}
+		if ( g_wpflags.integer & 512 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_PROX_LAUNCHER;
+			client->ps.ammo[ WP_PROX_LAUNCHER ] = getAmmoValue ( "g_startAmmoPL" );
+		}
+		if ( g_wpflags.integer & 1024 ) {
+			client->ps.stats[ STAT_WEAPONS ] |= 1 << WP_CHAINGUN;
+			client->ps.ammo[ WP_CHAINGUN ] = getAmmoValue ( "g_startAmmoCG" );
+		}
 #endif
+	}
 }
 
 /*
