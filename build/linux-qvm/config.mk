322303c48532e0afb48271ebd3b3044d30f30f84
Kr3m <kremisoski@gmail.com>
2024-04-20 05:39:29 -0400

    GPL conversion complete

--- build/linux-qvm/config.mk
+++ build/linux-qvm/config.mk
@@ -1,4 +1,4 @@
-PK3 = pak000.pk3
+PK3 = pak8a.pk3
 
 basedir = ../../code
 
@@ -6,8 +6,8 @@ QADIR = $(basedir)/game
 CGDIR = $(basedir)/cgame
 UIDIR = $(basedir)/q3_ui
 
-Q3ASM = q3asm -vq3 -r -m -v
-Q3LCC = q3lcc -DQ3_VM -S -Wf-g -I$(QADIR)
+Q3ASM = ./tools/q3asm -vq3 -r -m -v
+Q3LCC = ./../../tools/q3lcc -DQ3_VM -S -Wf-g -I$(QADIR)
 7Z = 7z u -tzip -mx=9 -mpass=8 -mfb=255 --
 
 QA_CFLAGS = -DQAGAME
